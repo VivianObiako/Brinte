08901dd66163bc3daf53052be3936af771674383
VivianObiako <vivianobiako1@gmail.com>
2024-08-14 12:34:57 +0100

    feat: added font, assets and configured tailwind css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,38 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        dark: { 600: "#303032" },
+        text: { 
+          primary: "#202022", 
+          secondary: "#5E5E60", 
+          subdued: "#909092", 
+          info: "#1A6AED", 
+          label: "#464649", 
+          neutral: "#F2F2F3",
+          cta: "#FEFEFF" ,
+          error: "#FF401E"
+        },
+        primary: { 100: "#824FE7" },
+        orange: { 400: "#FFB693" },
+        purple: { 400: "#D2BCFF" },
+        bg: { invert: "#F2F2F3", overlay: "#46464929" },
+        surface: { 
+          active: "#F2F2F3", 
+          bgInvert: "#FEFEFF", 
+          info: "#EAF3FF", 
+          neutral: "#202022",
+          subdue: "#E6E6E7"
+        },
+        border: {
+          light: "rgba(218, 218, 219, 1)",
+          error: "rgba(255, 64, 30, 1)",
+        }
+      },
+      boxShadow: {
+        'active-input': '0px 0px 0px 4px #8957EF33',
+        'card': '0px 0.5px 0px 0px rgba(86, 94, 113, 0.08) 0px 0.5px 0px 0px rgba(63, 71, 89, 0.12) 0px 1px 0.5px 0px rgba(0, 0, 0, 0.16) ',
+        'error': '0px 0px 0px 4px rgba(255, 64, 30, 0.24)',
       },
     },
   },
